441febb16424f170b4c0dfae8177ea59fa9751ac
jinwook jung <jinwook@pop-os.localdomain>
2021-10-19 01:24:58 +0900

    [A11] AppLoading

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	// preload
+	const [loading, setLoading] = useState(true);
+	const onFinish = () => setLoading(false);
+	const preload = () => {
+		const fontsToLoad = [Ionicons.font];
+		console.log(fontsToLoad);
+		const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+
+		const imagesToLoad = [
+			"https://www.pinclipart.com/picdir/middle/444-4443670_free-png-coffee-logo-image-png-images-transparent.png",
+		];
+		const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+		return Promise.all([...fontPromises, ...imagePromises]);
+	};
+	if (loading) {
+		return (
+			<AppLoading
+				startAsync={preload}
+				onError={console.warn}
+				onFinish={onFinish}
+			/>
+		);
+	}
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
